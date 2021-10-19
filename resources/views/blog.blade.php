3f4818f207649c57fc05d0c5a6c8b779a930b441
Rahul Sharma <rahulkandel7@gmail.com>
2021-10-19 15:23:34 +0545

    removed blog posts

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -32,72 +32,7 @@
                 </p>
             </div>
 
-            <div class="shadow-lg hover:shadow-xl cursor-pointer w-72 mb-2 p-5 mr-2 service">
-                <img src="{{asset('images/background/hero.png')}}" alt="">
-                <p class="text-black font-bold text-xl mt-2">
-                    Here will be Title
-                </p>
-                <p class="text-gray-500 text-justify max-h-24 overflow-hidden">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores sequi reprehenderit nihil at suscipit? Ab facilis assumenda officiis modi cumque provident saepe laudantium harum possimus! Dolorum assumenda enim autem ullam.
-                </p>
-                <p class="text-gray-500 text-sm mt-2 font-semibold">
-                    2021/12/30
-                </p>
-            </div>
             
-
-            <div class="shadow-lg hover:shadow-xl cursor-pointer w-72 mb-2 p-5 mr-2 service">
-                <img src="{{asset('images/background/hero.png')}}" alt="">
-                <p class="text-black font-bold text-xl mt-2">
-                    Here will be Title
-                </p>
-                <p class="text-gray-500 text-justify max-h-24 overflow-hidden">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores sequi reprehenderit nihil at suscipit? Ab facilis assumenda officiis modi cumque provident saepe laudantium harum possimus! Dolorum assumenda enim autem ullam.
-                </p>
-                <p class="text-gray-500 text-sm mt-2 font-semibold">
-                    2021/12/30
-                </p>
-            </div>
-            
-
-            <div class="shadow-lg hover:shadow-xl cursor-pointer w-72 mb-2 p-5 mr-2 service">
-                <img src="{{asset('images/background/hero.png')}}" alt="">
-                <p class="text-black font-bold text-xl mt-2">
-                    Here will be Title
-                </p>
-                <p class="text-gray-500 text-justify max-h-24 overflow-hidden">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores sequi reprehenderit nihil at suscipit? Ab facilis assumenda officiis modi cumque provident saepe laudantium harum possimus! Dolorum assumenda enim autem ullam.
-                </p>
-                <p class="text-gray-500 text-sm mt-2 font-semibold">
-                    2021/12/30
-                </p>
-            </div>
-            
-            <div class="shadow-lg hover:shadow-xl cursor-pointer w-72 mb-2 p-5 mr-2 service">
-                <img src="{{asset('images/background/hero.png')}}" alt="">
-                <p class="text-black font-bold text-xl mt-2">
-                    Here will be Title
-                </p>
-                <p class="text-gray-500 text-justify max-h-24 overflow-hidden">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores sequi reprehenderit nihil at suscipit? Ab facilis assumenda officiis modi cumque provident saepe laudantium harum possimus! Dolorum assumenda enim autem ullam.
-                </p>
-                <p class="text-gray-500 text-sm mt-2 font-semibold">
-                    2021/12/30
-                </p>
-            </div>
-            
-            <div class="shadow-lg hover:shadow-xl cursor-pointer w-72 mb-2 p-5 mr-2 service">
-                <img src="{{asset('images/background/hero.png')}}" alt="">
-                <p class="text-black font-bold text-xl mt-2">
-                    Here will be Title
-                </p>
-                <p class="text-gray-500 text-justify max-h-24 overflow-hidden">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores sequi reprehenderit nihil at suscipit? Ab facilis assumenda officiis modi cumque provident saepe laudantium harum possimus! Dolorum assumenda enim autem ullam.
-                </p>
-                <p class="text-gray-500 text-sm mt-2 font-semibold">
-                    2021/12/30
-                </p>
-            </div>
             
 
         </div>
