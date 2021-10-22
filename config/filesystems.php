aa786841670a8da6a14a94ba9deb495909a8d262
Rahul Sharma <rahulkandel7@gmail.com>
2021-10-22 19:26:57 +0545

    path

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,7 +55,7 @@ return [
 
         'my' => [
             'driver' => 'local',
-            'root'   => public_path('app/public_html/storage'),
+            'root'   => base_path('app/public_html/storage'),
             'visibility' => 'public',
         ],
     ],
